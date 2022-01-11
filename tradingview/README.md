31b3eee826fb442dd3872ea68847987983ea5edd
stewarteaton <stewarteaton05@gmail.com>
2022-01-11 14:29:20 -0500

    kucoin & trading view init

---
+++ tradingview/README.md
@@ -0,0 +1,17 @@
+<h1>Automated Trading Bots</h1>
+</br>
+
+<h3>Trading View</h3>
+Website where you can apply code to stock/crypto charts in order to perform technical analysis.
+Alerts can be configured and sent when programmatic conditions are met.
+</br>
+
+<h3>Web Hooks</h3>
+Using the alerts from trading view, we can feed the notifications(ex. buy/sell orders) to an API which triggers buys and sells on a crypto/stock exchage.. Hence ("Trading Bots")
+</br>
+
+<h3>Benefit of Trading View</h3>
+Instead of having to host a server that's constantly pulling stock/crypto chart data for assets on everytime frame and having to constantly run to check if indicators are met, Trading View provides this funtionality for nearly free. There is a huge open source community on Trading View to discuess and share strategy and scripts. Trading Views backtesting function is extremely useful as well. 
+</br>
+
+<h1>Heroku: Hosting the App</h1>
---
+++ tradingview/app.py
@@ -0,0 +1,57 @@
+from flask import Flask, request
+import json
+import config
+from binance.client import Client
+from binance.clientPaper import ClientPaper
+from binance.enums import *
+
+
+app = Flask(__name__)
+
+clientPaper = ClientPaper(config.API_KEY_PAPER, config.SECRET_KEY_PAPER, tld='us')
+clientReal = Client(config.API_KEY, config.SECRET_KEY, tld='us')
+
+info = clientReal.get_symbol_info('ETHUSDT')
+print(info['filters'][2]['minQty'])
+
+
+def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
+    try:
+        print(f"sending over {order_type} - {side} {quantity} {symbol}")
+        order = clientPaper.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
+        # order = clientReal.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
+        print(order)
+    except Exception as e:
+        print("an exception occured - {}".format(e))
+        return False
+    
+    return True
+
+@app.route('/')
+def hello_world(): 
+    return "Hi !"
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    # convert json to python dictionary    
+    data = json.loads(request.data)
+
+    if data["passphrase"] != config.WEBHOOK_PASSPHRASE:
+        return {
+            "code": "error",
+            "message": "Invalid passphrase"
+        }
+    
+    print(data['ticker'])
+    print(data['bar'])
+
+    side = data['strategy']['order_action'].upper() # upper makes all caps
+    
+
+    order_response = order("BUY", .1, "ETHUSDT")
+    print(order_response)
+
+    return {
+        "code": "success",
+        "message": data
+    }
